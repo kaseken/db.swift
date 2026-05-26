f3174e68ecbd2340fc0927d91f6a7e836c7fdf73
kaseken <kent.kaseda.dev@gmail.com>
2026-05-27 00:49:07 +0900

    refactoring

--- Sources/DatabaseCore/Btree/BTree.swift
+++ Sources/DatabaseCore/Btree/BTree.swift
@@ -89,23 +89,8 @@ class BTree {
 
     // MARK: - Debug
 
-    func printTree(pageNum: UInt32 = 0, indentation: Int = 0) {
-        let indent = String(repeating: "  ", count: indentation)
-        switch BTreeNodeFactory.restore(from: try! pager.getPage(Int(pageNum))) {
-        case let .leaf(node):
-            print("\(indent)- leaf (size \(node.cells.count))")
-            for i in 0 ..< node.cells.count {
-                print("\(indent)  - \(node.key(at: i))")
-            }
-        case let .internal(node):
-            print("\(indent)- internal (size \(node.cells.count))")
-            for i in 0 ..< node.cells.count {
-                let childPageNum = node.childPageNum(at: i)
-                printTree(pageNum: childPageNum, indentation: indentation + 1)
-                print("\(indent)  - key \(node.key(at: i))")
-            }
-            printTree(pageNum: node.rightmostChildPageNum, indentation: indentation + 1)
-        }
+    func printTree() {
+        BTreePrinter(pager: pager).printTree()
     }
 
     // MARK: - Private row access
---
+++ Sources/DatabaseCore/Btree/BTreePrinter.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+struct BTreePrinter {
+    private let pager: Pager
+
+    init(pager: Pager) {
+        self.pager = pager
+    }
+
+    func printTree(pageNum: UInt32 = 0, indentation: Int = 0) {
+        let indent = String(repeating: "  ", count: indentation)
+        switch BTreeNodeFactory.restore(from: try! pager.getPage(Int(pageNum))) {
+        case let .leaf(node):
+            print("\(indent)- leaf (size \(node.cells.count))")
+            for i in 0 ..< node.cells.count {
+                print("\(indent)  - \(node.key(at: i))")
+            }
+        case let .internal(node):
+            print("\(indent)- internal (size \(node.cells.count))")
+            for i in 0 ..< node.cells.count {
+                let childPageNum = node.childPageNum(at: i)
+                printTree(pageNum: childPageNum, indentation: indentation + 1)
+                print("\(indent)  - key \(node.key(at: i))")
+            }
+            printTree(pageNum: node.rightmostChildPageNum, indentation: indentation + 1)
+        }
+    }
+}
